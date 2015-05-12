ec9778c277e7fa5d2be7eb10b85dcc83a62b591b
a1655647 <a1655647@localhost>
2015-05-12 11:47:18 -0300

    Prova01_001

--- src/main/java/Avaliacao1.java
+++ src/main/java/Avaliacao1.java
@@ -1,17 +1,25 @@
+
+import utfpr.ct.dainf.if62c.avaliacao.Complexo;
+
 /**
- * UTFPR - Universidade Tecnológica Federal do Paraná
- * DAINF - Departamento Acadêmico de Informática
- * IF62C - Fundamentos de Programação 2
- * 
+ * UTFPR - Universidade Tecnológica Federal do Paraná DAINF - Departamento
+ * Acadêmico de Informática IF62C - Fundamentos de Programação 2
+ *
  * Primeira avaliação parcial 2014/2.
- * @author 
+ *
+ * @author
  */
 public class Avaliacao1 {
 
     public static void main(String[] args) {
-        // implementar main
+
+
     }
-    
-    // implementar raizesEquacao(Complexo, Complexo, Complexo)
-    
+
+    public static Complexo[] raizesEquacao(Complexo a, Complexo b, Complexo c) {
+        
+        Complexo[] raizes = new Complexo[1];
+        
+        return raizes;
+    }
 }
--- src/main/java/utfpr/ct/dainf/if62c/avaliacao/Complexo.java
+++ src/main/java/utfpr/ct/dainf/if62c/avaliacao/Complexo.java
@@ -1,54 +1,69 @@
 package utfpr.ct.dainf.if62c.avaliacao;
 
 /**
- * UTFPR - Universidade Tecnológica Federal do Paraná
- * DAINF - Departamento Acadêmico de Informática
- * IF62C - Fundamentos de Programação 2
- * 
+ * UTFPR - Universidade Tecnológica Federal do Paraná DAINF - Departamento
+ * Acadêmico de Informática IF62C - Fundamentos de Programação 2
+ *
  * Primeira avaliação parcial 2014/2.
- * @author 
+ *
+ * @author
  */
 public class Complexo {
+
     private double real;
     private double img;
 
     public Complexo() {
     }
 
-    public Complexo(double real, double img) {
-        // completar a implementação
+    public Complexo(double real1, double img1) {
+        real = real1;
+        img = img1;
     }
 
-    // implementar getReal()
+    public double getReal() {
+        return real;
+    }
 
-    // implementar getImg()
+    public double getImg() {
+        return img;
+    }
 
     public Complexo soma(Complexo c) {
         return new Complexo(real + c.real, img + c.img);
     }
-    
-    // implementar sub(Complexo)
 
-    // implementar prod(double)
+    public Complexo sub(Complexo c) {
+        return new Complexo(real - c.real, img - c.img);
+    }
 
-    // implementar prod(Complexo)
-    
-    // implementar div(Complexo)
-    
+    public Complexo prod(double r) {
+        return new Complexo(r * real, r * img);
+    }
+
+    public Complexo prod(Complexo c) {
+        return new Complexo(real * c.real - img * c.img, real * c.real + img * c.img);
+    }
+
+    public Complexo div(Complexo c) {
+        return new Complexo(((real * c.real) + (img * c.img) / ((c.img * c.img) + (c.real * c.real))), (((c.real * img) - (real * c.img)) / ((c.img * c.img) + (c.real * c.real))));
+    }
+
     // implementar sqrt()
     public Complexo[] sqrt() {
-        // completar implementação
-        // retornar o vetor contendo as raízes
-        return null;
+        Complexo[] raizes = new Complexo[1];
+
+ 
+        return raizes;
     }
 
     @Override
     public int hashCode() {
         int hash = 5;
         hash = 41 * hash + (int) (Double.doubleToLongBits(real)
-            ^ (Double.doubleToLongBits(real) >>> 32));
+                ^ (Double.doubleToLongBits(real) >>> 32));
         hash = 41 * hash + (int) (Double.doubleToLongBits(img)
-            ^ (Double.doubleToLongBits(img) >>> 32));
+                ^ (Double.doubleToLongBits(img) >>> 32));
         return hash;
     }
 
@@ -56,7 +71,7 @@ public class Complexo {
     public boolean equals(Object obj) {
         final Complexo c = (Complexo) obj;
         return obj != null && getClass() == obj.getClass()
-            && real == c.real && img == c.img;
+                && real == c.real && img == c.img;
     }
 
     @Override
