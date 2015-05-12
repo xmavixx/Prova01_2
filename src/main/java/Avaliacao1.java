8bd465c024676c9d9476cd12f6f26f0f86bdb1f5
a1655647 <a1655647@localhost>
2015-05-12 12:03:05 -0300

    Prova01_002

--- src/main/java/Avaliacao1.java
+++ src/main/java/Avaliacao1.java
@@ -12,14 +12,31 @@ import utfpr.ct.dainf.if62c.avaliacao.Complexo;
 public class Avaliacao1 {
 
     public static void main(String[] args) {
-
-
+        Complexo a1, a2, b1, b2, c1, c2;
+        Complexo[] resposta1 = new Complexo[2];
+        Complexo[] resposta2 = new Complexo[2];
+        a1 = new Complexo(1, 0);
+        b1 = new Complexo(5, 0);
+        c1 = new Complexo(4, 0);
+        a2 = new Complexo(1, 0);
+        b2 = new Complexo(2, 0);
+        c2 = new Complexo(5, 0);
+        resposta1 = raizesEquacao(a1, b1, c1);
+        resposta2 = raizesEquacao(a2, b2, c2);
+        System.out.println("x1 = " + resposta1[0].toString() + "\n");
+        System.out.println("x2 = " + resposta1[1].toString() + "\n");
+        System.out.println("y1 = " + resposta2[0].toString() + "\n");
+        System.out.println("y2 = " + resposta2[1].toString() + "\n");
     }
 
     public static Complexo[] raizesEquacao(Complexo a, Complexo b, Complexo c) {
-        
-        Complexo[] raizes = new Complexo[1];
-        
-        return raizes;
+        Complexo[] vetor = new Complexo[2];
+        Complexo[] raiz = new Complexo[2];
+        Complexo delta = new Complexo();
+        delta = b.prod(b).sub(a.prod(c.prod(4)));
+        raiz = delta.sqrt();
+        vetor[0] = (b.prod(-1).soma(raiz[0])).div(a.prod(2));
+        vetor[1] = (b.prod(-1).soma(raiz[1])).div(a.prod(2));
+        return vetor;
     }
 }
